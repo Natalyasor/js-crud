02d696865391e7928e4335eb2442ea06cdaa723d
Natalyasor/SN <natalyasor110983@gmail.com>
2024-03-13 16:49:00 +0100

    2-product

--- src/route/index.js
+++ src/route/index.js
@@ -131,44 +131,50 @@ router.post('/user-update', function (req, res) {
 })
 
 // ================================================================
-class Product {
-  static #list = []
 
-  constructor(name, price, description) {
-    this.name = name
-    this.price = price
-    this.description = description
-    this.createDate = newDate()
-    this.id = Math.trunc(Math.random() * 100000)
-  }
+// router.get Створює нам один ентпоїнт
 
-  static add = (product) => {
-    this.#list.push(product)
-  }
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/product-list', function (req, res) {
+  // res.render генерує нам HTML сторінку
 
-  static getList = () => this.#list
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('product-list', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'index',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
 
-  static getById = (id) =>
-    this.#list.find((product) => product.id === id)
-
-  static deleteById = (id) => {
-    const index = this.#list.findIndex(
-      (product) => product.id === id,
-    )
-    if (index !== -1) {
-      this.#list.splice(index, 1)
-      return true
-    } else {
-      return false
-    }
-  }
-  static updateByid = (id, data) => {
-    const product = this.getById(id)
-  }
-}
 // ================================================================
 
-router.post('/product-create - alert', function (req, res) {
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/product-create', function (req, res) {
+  const list = Product.getList()
+
+  res.render('product-create', {
+    style: 'product-create',
+
+    data: {
+      Products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.post('/product-create', function (req, res) {
   const { name, price, description } = req.body
 
   const product = new Product(name, price, description)
@@ -177,12 +183,45 @@ router.post('/product-create - alert', function (req, res) {
 
   console.log(Product.getList())
 
-  res.render('alert', {
-    style: 'alert',
-    info_title: 'Успішно',
-    info_description: 'Товар додано',
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('product-create', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'product-create',
+
+    // ↑↑ сюди вводимо JSON дані
   })
 })
+// ================================================================
+class Product {
+  static #list = []
 
+  constructor(name, price, description) {
+    this.name = name
+    this.price = price
+    this.description = description
+  }
+
+  static add = (product) => {
+    this.#list.push(product)
+  }
+
+  static getList = () => {
+    return this.#list
+  }
+}
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.post('/product-list', function (req, res) {
+  const { name, price, description } = req.body
+  //
+  res.render('product-list', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'product-list',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
