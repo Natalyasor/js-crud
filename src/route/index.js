17af97da30ee334e1a6d5974de9901c95b3cba83
Natalyasor/SN <natalyasor110983@gmail.com>
2024-03-18 10:49:41 +0100

    3-purchase

--- src/route/index.js
+++ src/route/index.js
@@ -296,6 +296,141 @@ router.get('/product-delete', function (req, res) {
   })
 })
 // ================================================================
+// ================================================================
+
+class Purchase {
+  static #list = []
+
+  static #count = 0
+  constructor(img, title, description, category, price) {
+    this.id = ++Purchase.#count
+    this.img = img
+    this.title = title
+    this.description = description
+    this.category = category
+    this.price = price
+  }
+
+  static add = (
+    img,
+    title,
+    description,
+    category,
+    price,
+  ) => {
+    const newPurchase = new Purchase(
+      img,
+      title,
+      description,
+      category,
+      price,
+    )
+    this.#list.push(newPurchase)
+  }
+
+  static getList = () => {
+    return this.#list
+  }
+  static getById = (id) => {
+    return this.#list.find((purchase) => purchase.id === id)
+  }
+  static getRandomList = (id) => {
+    const filteredList = this.#list.filter(
+      (purchase) => purchase.id !== id,
+    )
+
+    const shuffledList = filteredList.sort(
+      () => Math.random() - 0.5,
+    )
+    return shuffledList.slice(0, 3)
+  }
+}
+Purchase.add(
+  '/https://picsum.photos/200/300',
+  `Комп'ютер Artline Gaming (X43v31) AMD Ryzen 5 3600/`,
+  `AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС`,
+  [
+    { id: 1, text: 'Готовий до відправки' },
+    { id: 2, text: 'Топ продажів' },
+  ],
+  27000,
+)
+Purchase.add(
+  '/https://picsum.photos/200/300',
+  `Комп'ютер COBRA Advanced (I11F.8.H1S2.15T.13356) Intel`,
+  `Intel Core i3-10100F (3.6 - 4.3 ГГц) / RAM 8 ГБ / HDD 1 ТБ + SSD 240 ГБ / GeForce GTX 1050 Ti, 4 ГБ / без ОД / LAN / Linux`,
+  [{ id: 2, text: 'Топ продажів' }],
+  17000,
+)
+Purchase.add(
+  '/https://picsum.photos/200/300',
+  `Комп'ютер ARTLINE Gaming by ASUS TUF v119 (TUFv119)`,
+  `Intel Core i9-13900KF (3.0 - 5.8 ГГц) / RAM 64 ГБ / SSD 2 ТБ (2 x 1 ТБ) / nVidia GeForce RTX 4070 Ti, 12 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / без ОС`,
+  [{ id: 2, text: 'Готовий до відправки' }],
+  113109,
+)
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/purchase-index', function (req, res) {
+  res.render('purchase-index', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'purchase-index',
+
+    data: {
+      list: Purchase.getList(),
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/purchase-product', function (req, res) {
+  const id = Number(req.query.id)
+  res.render('purchase-product', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'purchase-product',
+
+    data: {
+      list: Purchase.getRandomList(id),
+      purchase: Purchase.getById(id),
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.post('/purchase-create', function (req, res) {
+  const id = Number(req.query.id)
+  const amount = Number(req.body.amount)
+  console.log(id, amount)
+  res.render('purchase-product', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'purchase-product',
+
+    data: {
+      list: Purchase.getRandomList(id),
+      purchase: Purchase.getById(id),
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
 
 // Підключаємо роутер до бек-енду
 module.exports = router
